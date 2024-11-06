ae725fe0f5de8911668fff2e6e82164aaa1116b3
Ayb001 <default@defaults-MacBook-Pro.local>
2024-11-06 20:36:13 +0100

    split function

--- ft_split.c
+++ ft_split.c
@@ -1,2 +1,85 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ayaghjed <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/03 19:19:27 by ayaghjed          #+#    #+#             */
+/*   Updated: 2024/11/03 19:19:32 by ayaghjed         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static int count_word(const char *s,char c)
+{
+    int i;
+    int count;
+
+    count =0;
+    i=0;
+    while(s[i]!='\0')
+    {
+        while(s[i]==c)
+            i++;
+        while(s[i]==c)
+        {
+            i++;
+            if((s[i]==c && s[i+1]!=c) || (s[i+1]=='\0'))
+            {
+                count++;
+            }
+        }
+        i++;
+    }
+    return (count);
+}
+static char *split_copy(const char *s,int len_word)
+{
+    int i;
+    char *str;
+
+    i=0;
+    str=malloc(sizeof(char *)*(len_word+1));
+    if (!str)
+    {
+        return (NULL);
+    }
+    while(i<len_word)
+    {
+        str[i]=s[i];
+        i++;
+    }
+    str[i]='\0';
+    return (str);
+}
 char **ft_split(char const *s, char c)
-{}
+{
+    int i;
+    int j;
+    int len_word;
+    char **result;
+
+    if(!s)
+        return (NULL);
+    len_word=count_word(s,c);
+    result=malloc(sizeof(char *) * (len_word+1));
+    if(!result)
+        return NULL;
+    i=0;
+    j=0;
+    while(s[i]!='\0')
+    {
+        while(s[i]==c)
+            i++;
+        if((s[i+1]==c && (s[i+1]== c) || s[i+1] == '\0'))
+        {
+            result[j]=split_copy(s+i,len_word);
+            j++
+        }
+        i++;
+    }
+    result[j]=NULL;
+    return (result);
+}
--- libft.h
+++ libft.h
@@ -32,6 +32,8 @@ char	*ft_strchr(const char *s, int c);
 char	*ft_strdup(const char *s1);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
+char **ft_split(char const *s, char c);
+char *ft_strnstr(const char *haystack, const char *needle, size_t len);
 void	*ft_memset(void *b, int c, size_t len);
 void	ft_putnbr_fd(int n, int fd);
 void	ft_putendl_fd(char *s, int fd);
