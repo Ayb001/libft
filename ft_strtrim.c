15a1ba800f6b4dc6784c2f9804ff6c2971448dd6
Ayb001 <default@defaults-MacBook-Pro.local>
2024-12-03 20:25:09 +0100

    libft project

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -12,60 +12,22 @@
 
 #include "libft.h"
 
-static int  *hash_table(int *check_tab, const char *set)
+char	*ft_strtrim(char const *s1, char const *set)
 {
-    int i;
+	char	*trimmed;
+	size_t	start;
+	size_t	end;
 
-    i = 0;
-    if (!check_tab)
-        return (0);
-    ft_memset(check_tab, 0, 255);
-    while (set[i] != 0) {
-        check_tab[(unsigned char)set[i]] = 1;
-        i++;
-    }
-    return (check_tab);
+	if (!s1)
+		return (NULL);
+	if (!set)
+		return (ft_strdup(s1));
+	end = ft_strlen(s1) - 1;
+	start = 0;
+	while (s1[start] && ft_strchr(set, s1[start]))
+		++start;
+	while (s1[end] && ft_strchr(set, s1[end]))
+		--end;
+	trimmed = ft_substr(s1, start, end - start + 1);
+	return (trimmed);
 }
-
-static char *ft_set_str(int i, int j, const char *s1)
-{
-    char    *str;
-
-    if (i > j) {
-        str = (char *)malloc(1);
-        if (!str)
-            return (0);
-        str[0] = 0;
-        return (str);
-    }
-    str = (char *)malloc(j - i + 2);
-    if (!str)
-        return (0);
-    ft_memcpy(str, s1 + i, j - i + 1);
-    str[j - i + 1] = 0;
-    return (str);
-}
-
-char    *ft_strtrim(char const *s1, char const *set)
-{
-    int     check_tab[255];
-    int     i;
-    int     j;
-    char    *str;
-
-    if (!s1)
-        return (0);
-    if (!set)
-        return (ft_strdup((const char *)s1));
-    hash_table(check_tab, set);
-    i = ft_strlen(s1) - 1;
-    while (check_tab[(unsigned char)s1[i]] == 1)
-        i--;
-    j = i;
-    i = 0;
-    while (check_tab[(unsigned char)s1[i]] == 1)
-        i++;
-    str = ft_set_str(i, j, s1);
-    return (str);
-}
-
